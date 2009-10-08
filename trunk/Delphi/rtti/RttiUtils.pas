eadace80bebf3624a3f775f1b2f8c81c364992f3
rlove@peakbiz.com <rlove@peakbiz.com@1cf5dbae-285b-11de-a7d5-23715972da18>
2009-10-08 22:40:28 +0000

    Changes to make Attributes work correctly in Xml Serialization.

--- trunk/Delphi/rtti/RttiUtils.pas
+++ trunk/Delphi/rtti/RttiUtils.pas
@@ -23,7 +23,7 @@ unit RttiUtils;
 //
 
 interface
-uses SysUtils,Classes,Rtti;
+uses SysUtils,Classes,Rtti,TypInfo;
 type
   ERttiMemberHelperException = class(Exception);
   // Make things a bit easier.
@@ -39,6 +39,21 @@ type
     property MemberType : TRttiType read GetType;
   end;
 
+  TCustomAttributeClass = class of TCustomAttribute;
+
+  TAttrUtils = class(TObject)
+   public
+     class function HasAttribute(aType : pTypeinfo;aClass : TCustomAttributeClass;var Attr : TCustomAttribute) : Boolean; overload;
+     class function HasAttribute(aContext : TRttiContext; aType : TRttiObject;aClass : TCustomAttributeClass;var Attr : TCustomAttribute) : Boolean; overload;
+     class function HasAttributes(aType : pTypeinfo;aClass : TCustomAttributeClass;var Attrs : TArray<TCustomAttribute>) : Boolean; overload;
+     class function HasAttributes(aContext : TRttiContext; aType : TRttiObject;aClass : TCustomAttributeClass;var Attrs : TArray<TCustomAttribute>) : Boolean; overload;
+
+     class function GetAttribute(aType : pTypeinfo;aClass : TCustomAttributeClass) :  TCustomAttribute; overload;
+     class function GetAttribute(aContext : TRttiContext; aType : TRttiObject;aClass : TCustomAttributeClass): TCustomAttribute ; overload;
+
+     class function GetAttributes(aType : pTypeinfo;aClass : TCustomAttributeClass) :  TArray<TCustomAttribute>; overload;
+     class function GetAttributes(aContext : TRttiContext; aType : TRttiObject;aClass : TCustomAttributeClass): TArray<TCustomAttribute> ; overload;
+  end;
 
 
 implementation
@@ -101,5 +116,110 @@ begin
     SetValue(Instance.GetReferenceToRawData,aValue)
   end;
 end;
+
+class function TAttrUtils.GetAttribute(aType: pTypeinfo;
+  aClass: TCustomAttributeClass): TCustomAttribute;
+var
+ c : TRttiContext;
+begin
+ c := TRttiContext.Create;
+ try
+   result := GetAttribute(c, c.GetType(aType),aClass);
+ finally
+   c.Free;
+ end;
+end;
+
+class function TAttrUtils.GetAttribute(aContext: TRttiContext; aType: TRttiObject;
+  aClass: TCustomAttributeClass): TCustomAttribute;
+var
+ lAttr : TCustomAttribute;
+begin
+  Assert(Assigned(aType));
+  for lAttr in aType.GetAttributes do
+  begin
+    if lAttr is aClass then
+    begin
+      exit(lAttr);
+    end;
+  end;
+  result := nil;
+end;
+
+class function TAttrUtils.GetAttributes(aContext: TRttiContext;
+  aType: TRttiObject; aClass: TCustomAttributeClass): TArray<TCustomAttribute>;
+var
+  Attrs : TArray<TCustomAttribute>;
+  lp,idx : Integer;
+begin
+  Assert(Assigned(aType));
+  Attrs := aType.GetAttributes;
+  SetLength(result,Length(Attrs));
+  idx := 0;
+  for lp := 0 to Length(Attrs) - 1 do
+  begin
+    if Attrs[lp] is aClass then
+    begin
+      result[idx] := Attrs[lp];
+      inc(idx);
+    end;
+  end;
+  SetLength(result,idx);
+end;
+
+class function TAttrUtils.GetAttributes(aType: pTypeinfo;
+  aClass: TCustomAttributeClass): TArray<TCustomAttribute>;
+var
+ c : TRttiContext;
+begin
+ c := TRttiContext.Create;
+ try
+   result := GetAttributes(c, c.GetType(aType),aClass);
+ finally
+   c.Free;
+ end;
+end;
+
+class function TAttrUtils.HasAttribute(aType: pTypeinfo;
+  aClass: TCustomAttributeClass; var Attr: TCustomAttribute): Boolean;
+var
+ c : TRttiContext;
+begin
+ c := TRttiContext.Create;
+ try
+   result := HasAttribute(c, c.GetType(aType),aClass,Attr);
+ finally
+   c.Free;
+ end;
+end;
+
+class function TAttrUtils.HasAttribute(aContext: TRttiContext; aType: TRttiObject;
+  aClass: TCustomAttributeClass; var Attr: TCustomAttribute): Boolean;
+begin
+  Attr := GetAttribute(aContext,aType,aClass);
+  result := Assigned(Attr);
+end;
+
+class function TAttrUtils.HasAttributes(aContext: TRttiContext;
+  aType: TRttiObject; aClass: TCustomAttributeClass;
+  var Attrs: TArray<TCustomAttribute>): Boolean;
+begin
+  Attrs := GetAttributes(aContext,aType,aClass);
+  result := Length(Attrs) > 0;
+end;
+
+class function TAttrUtils.HasAttributes(aType: pTypeinfo;
+  aClass: TCustomAttributeClass; var Attrs: TArray<TCustomAttribute>): Boolean;
+var
+ c : TRttiContext;
+begin
+ c := TRttiContext.Create;
+ try
+   result := HasAttributes(c, c.GetType(aType),aClass,Attrs);
+ finally
+   c.Free;
+ end;
+end;
+
 
 end.
