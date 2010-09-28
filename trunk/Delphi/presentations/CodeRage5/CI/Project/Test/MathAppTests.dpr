96d4d9bea5fb0e7edf0116afe15adf6c4e5a87cd
rlove@peakbiz.com <rlove@peakbiz.com@1cf5dbae-285b-11de-a7d5-23715972da18>
2010-09-28 19:31:43 +0000

    CI Examples

---
+++ trunk/Delphi/presentations/CodeRage5/CI/Project/Test/MathAppTests.dpr
@@ -0,0 +1,46 @@
+program MathAppTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  FastMM4,
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  TestMathClass in 'TestMathClass.pas',
+  MathClass in '..\MathClass.pas',
+  XMLTestRunner in 'C:\Program Files\Embarcadero\RAD Studio\8.0\source\dUnit\Contrib\XMLReporting\XMLTestRunner.pas';
+
+{$R *.RES}
+
+var
+ XmlFile : String;
+begin
+  Application.Initialize;
+  if IsConsole then
+  begin
+      if ParamCount = 1 then
+        XmlFile := ParamStr(1)
+      else
+        XmlFile := DEFAULT_FILENAME;
+      XMLTestRunner.RunRegisteredTests(XmlFile);
+  //  with TextTestRunner.RunRegisteredTests do
+  //    Free
+  end
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
